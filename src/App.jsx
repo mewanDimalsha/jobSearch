8deb84273bbf6dc7373e2dd0bef40ae706db7151
Mewan Dimalsha <115539818+mewanDimalsha@users.noreply.github.com>
2023-11-04 19:17:56 +0530

    `half completed NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBar from './componenets/NavBar/NavBar'
 import Search from './componenets/SearchDiv/Search'
+import Jobs from './componenets/JobDiv/Jobs'
+import Value from './componenets/ValueDiv/Value'
+import Footer from './componenets/FooterDiv/Footer'
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Search />
+      <Jobs />
+      <Value />
+      <Footer />
     </div>
   )
 }
